21d04eac7804b6cf14bafad0b815284e19d1aa05
Ayomide Osineye <ayomideosineye1@gmail.com>
2026-09-22 18:11:36 +0100

    continued to refactor code, adding functions such as total_stars, average_stars, and load_data. Modularised the code further removing redundant code

--- api_project_structured.py
+++ api_project_structured.py
@@ -11,10 +11,27 @@ def get_username():
     username = input("Enter the username to get their repos: ")
     return username
 
+
 # function that gets the API request and returns the repositories
 def get_repositories(username: str):
     get_repos = requests.get(f"https://api.github.com/users/{username}/repos")
     return get_repos
+
+
+# function that loads valid repository data for a username 
+def load_data():
+
+    # requested data is initially False
+    requested_data = False
+
+    # loops while the requested data is invalid
+    while requested_data is False:
+        username = get_username()
+        get_repos = get_repositories(username)
+        requested_data = valid_request(get_repos)
+
+    return username, requested_data
+
 
 # function that checks whether the retrieved data is valid
 def valid_request(data: requests.models.Response):
@@ -27,11 +44,15 @@ def valid_request(data: requests.models.Response):
         print("###################################################", '\n')
         return False
 
+
 # function that returns the languages used and their count, within the users repositories
 def languages_count(repos_data):
 
     # languages dict
     lang_dict = {}
+
+    if len(repos_data) == 0:
+        return "N/A"
 
     for repo in repos_data:
         # check if the language used given the repo is not "null"
@@ -43,12 +64,17 @@ def languages_count(repos_data):
             else:
             # create the language entry
                 lang_dict[repo["language"]] = 1
+
     return lang_dict
 
-# returns the repository nmae and its star count.
+
+# returns the repository name and its star count.
 def highest_stars(repos_data):
     current_highest = 0
     repo_name = ""
+
+    if len(repos_data) == 0:
+        return "N/A"
 
     # loop through each repo in the repos_list
     for repo in repos_data:
@@ -57,31 +83,58 @@ def highest_stars(repos_data):
             current_highest = repo["stargazers_count"]
             repo_name = repo["name"]
 
-    return (repo_name, current_highest) 
+    return (repo_name, current_highest)
 
 
-        
-            
+# get the total starts given a user 
+def total_stars(repos_data):
 
-# initially there is no requested data so False
-requested_data = False
-username = ""
+    # initialise inital total stars
+    total_stars = 0
 
-# loops while the requested data is invalid
-while requested_data is False:
-    username = get_username()
-    get_repos = get_repositories(username)
-    requested_data = valid_request(get_repos)
+    # loop through each repo belonging to the user 
+    # and add each repos stars together
+    for repo in repos_data:
+        total_stars += repo["stargazers_count"]
+
+    return total_stars
+
+
+def average_stars(repos_data):
+
+    # check if the user actually has repositories
+    if len(repos_data) == 0:
+        return "N/A"
+
+    # get the total stars given the user
+    user_stars = total_stars(repos_data)
+ 
+    # get the average stars per repo
+    average  = user_stars / len(repos_data)
+
+    return average
+
+
+
+# start the inital program, getting the username and repo_data
+username, repos_data = load_data()
 
 # get the languages used by the user given their repos
-language_count = languages_count(requested_data)
+language_count = languages_count(repos_data)
 print(f"languages used and count for user '{username}' : ",language_count)
 
 # get the repository with the highest star count
-highest_star_repo = highest_stars(requested_data)
+highest_star_repo = highest_stars(repos_data)
 print("Repo with the highest stars:", highest_star_repo)
 
+# get the total stars the user has 
+total = total_stars(repos_data)
+print(f"{username}'s has a total of {total} stars")
 
+# get the average stars 
+average = average_stars(repos_data)
+
+print(f"{username}'s average stars per repo is {average}")
 
 
 
